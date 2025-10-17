e306678243f2fdc1965dd4491f19846f3772cfb6
himanushi <himanushi@users.noreply.github.com>
2025-10-18 00:06:18 +0900

    add: main.gd

---
+++ godot-project/main.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+# メインゲームスクリプト
+# Scribble LLM Game - LLMを使った2Dパズルゲーム
+
+func _ready():
+	print("Scribble LLM Game が起動しました！")
+	print("Godot ", Engine.get_version_info().string)
+
+func _process(_delta):
+	pass
